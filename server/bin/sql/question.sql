741eb920255ad9f187a07ff901db4e5c64727b13
Carol Chung <cch5ng@gmail.com>
2019-01-19 15:52:10 -0800

    change keyword order to sort_order

--- server/bin/sql/question.sql
+++ server/bin/sql/question.sql
@@ -1,7 +1,6 @@
-#list of original questions
 CREATE TABLE question(
-	id 				SERIAL PRIMARY KEY,
-	content 	TEXT,
-	category 	VARCHAR(64),
-	order 		INTEGER
+	id 					SERIAL PRIMARY KEY,
+	content 		TEXT,
+	category 		VARCHAR(64),
+	sort_order 	INTEGER
 );
