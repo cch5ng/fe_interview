9cfdebe272eb48e29e299df5814629e9f8f5521c
Carol Chung <cch5ng@gmail.com>
2019-01-19 15:52:20 -0800

    change keyword order to sort_order

--- server/bin/sql/question_list_custom_question.sql
+++ server/bin/sql/question_list_custom_question.sql
@@ -1,8 +1,8 @@
 CREATE TABLE question_list_custom_question(
-	id SERIAL PRIMARY KEY,
-	question_list_custom_id INTEGER,
-	question_id INTEGER,
-	order INTEGER,
-	FOREIGN KEY question_list_custom_id REFERENCES question_list_custom(id), 
-	FOREIGN KEY question_id REFERENCES question(id)
+	id 												SERIAL PRIMARY KEY,
+	sort_order 								INTEGER,
+	question_list_custom_id 	INTEGER,
+	question_id 							INTEGER,
+	FOREIGN KEY (question_list_custom_id) REFERENCES question_list_custom(id), 
+	FOREIGN KEY (question_id) REFERENCES question(id)
 );
