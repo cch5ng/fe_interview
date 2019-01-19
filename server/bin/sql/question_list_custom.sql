041619fbbcba4a567c572466dc985cab4a0bd38a
Carol Chung <cch5ng@gmail.com>
2019-01-19 15:52:37 -0800

    update app user table

--- server/bin/sql/question_list_custom.sql
+++ server/bin/sql/question_list_custom.sql
@@ -1,7 +1,6 @@
-# custom list that user creates for testing
 CREATE TABLE question_list_custom(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(64),
 	user_id INTEGER,
-	FOREIGN KEY (user_id) REFERENCES user(id)
+	FOREIGN KEY (user_id) REFERENCES fe_user(id)
 );
