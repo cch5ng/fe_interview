c35a8caffe19ceb8001f794d1c899e323d94b41b
Carol Chung <cch5ng@gmail.com>
2019-01-19 15:51:17 -0800

    rename app user table

--- server/bin/configure_db.sh
+++ server/bin/configure_db.sh
@@ -5,7 +5,7 @@ echo 'Configuring fe_interview_db'
 dropdb fe_interview_db
 createdb fe_interview_db
 
-psql fe_interview_db < ./bin/sql/user.sql
+psql fe_interview_db < ./bin/sql/fe_user.sql
 psql fe_interview_db < ./bin/sql/question.sql
 psql fe_interview_db < ./bin/sql/question_list_custom.sql
 psql fe_interview_db < ./bin/sql/question_list_custom_question.sql
