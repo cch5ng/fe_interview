5d86c954108d9459b0c75e1b0d881a6812de67c3
Carol Chung <cch5ng@gmail.com>
2019-01-21 11:57:22 -0800

    updated cur db user/owner name

--- server/bin/configure_db.sh
+++ server/bin/configure_db.sh
@@ -2,12 +2,12 @@
 
 echo 'Configuring fe_interview_db'
 
-dropdb fe_interview_db
-createdb fe_interview_db
+dropdb -U fe_interview_user fe_interview_db
+createdb -U fe_interview_user fe_interview_db
 
-psql fe_interview_db < ./bin/sql/fe_user.sql
-psql fe_interview_db < ./bin/sql/question.sql
-psql fe_interview_db < ./bin/sql/question_list_custom.sql
-psql fe_interview_db < ./bin/sql/question_list_custom_question.sql
+psql -U fe_interview_user fe_interview_db < ./bin/sql/fe_user.sql
+psql -U fe_interview_user fe_interview_db < ./bin/sql/question.sql
+psql -U fe_interview_user fe_interview_db < ./bin/sql/question_list_custom.sql
+psql -U fe_interview_user fe_interview_db < ./bin/sql/question_list_custom_question.sql
 
 echo 'fe_interview_db configured'
