fcd4596e7378c8aace2864ddc714d26dff915a79
Carol Chung <cch5ng@gmail.com>
2019-01-21 16:23:33 -0800

    added a test

--- server/databasePool.js
+++ server/databasePool.js
@@ -4,3 +4,10 @@ const databaseConfiguration = require('./secrets/databaseConfiguration');
 const pool = new Pool(databaseConfiguration);
 
 module.exports = pool;
+
+// test pool connection
+// pool.query('SELECT * from question', (err, resp) => {
+// 	if (err) return console.log('error', err);
+
+// 	console.log('response.rows', resp.rows);
+// })
