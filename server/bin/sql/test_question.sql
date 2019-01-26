a0f736248e32466e2d10641663d0f070e93e1cb1
Carol Chung <cch5ng@gmail.com>
2019-01-25 19:59:28 -0800

    update test_question table to include question_completed and needs_review fields

--- server/bin/sql/test_question.sql
+++ server/bin/sql/test_question.sql
@@ -1,6 +1,8 @@
 CREATE TABLE test_question(
 	test_id INTEGER,
 	question_id INTEGER,
+	question_completed BOOLEAN,
+	needs_review BOOLEAN,
 	FOREIGN KEY (test_id) REFERENCES test(id),
 	FOREIGN KEY (question_id) REFERENCES question(id)
 );
