c0532f30dc28e822f43894b28018d77b160c9c56
Carol Chung <cch5ng@gmail.com>
2019-01-26 12:41:00 -0800

    add getQuestionsByTestId() funct

--- server/app/testQuestion/table.js
+++ server/app/testQuestion/table.js
@@ -1,8 +1,8 @@
 const pool = require('../../databasePool');
 
 class TestQuestionTable {
-	static storeTestQuestion({ test_id, question_id, question_completed, needs_review }) {
 
+	static storeTestQuestion({ test_id, question_id, question_completed, needs_review }) {
 		return new Promise((resolve, reject) => {
 			pool.query(
 				`INSERT INTO test_question(test_id, question_id, question_completed, needs_review)
@@ -16,6 +16,27 @@ class TestQuestionTable {
 			)
 		})
 	}
+
+	// needs to return at least the question id and question content
+	static getQuestionsByTestId({ test_id }) {
+		return new Promise((resolve, reject) => {
+			pool.query(
+				`SELECT question.id, question.content, test_question.question_completed, test_question.needs_review from test_question
+					INNER JOIN question 
+					ON question.id = test_question.question_id
+					WHERE test_question.test_id = $1`,
+				[test_id],
+				(err, resp) => {
+					if (err) return reject(err);
+
+					console.log('questions', resp.rows);
+
+					resolve(resp.rows);
+				}
+			);
+		});
+	}
+
 }
 
 module.exports = TestQuestionTable;
