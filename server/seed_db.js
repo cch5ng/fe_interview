d36e472c06bdfddc43b2b7a60d148c12d30ad7db
Carol Chung <cch5ng@gmail.com>
2019-01-28 11:57:32 -0800

    add test data to seed script

--- server/seed_db.js
+++ server/seed_db.js
@@ -1,5 +1,7 @@
 const QuestionTable = require('./app/question/table');
+const TestTable = require('./app/test/table');
 
+// seed questions
 const JavaScriptQuestions = require('./question_output/javascript-questions');
 const CssQuestions = require('./question_output/css-questions');
 const FunQuestions = require('./question_output/fun-questions');
@@ -32,5 +34,48 @@ outputObjects.forEach(qObj => {
 				.catch(err => console.error('error', err))				
 	});
 });
+
+// seed tests
+let dummyDate = new Date();
+
+let dummyQuestions = [
+		{	content: 'Explain how ^this^ works in JavaScript',
+			question_completed: false,
+			needs_review: true,
+			question_id: 2
+		},
+		{	content: "Explain why the following doesn't work as an IIFE: ^function foo(){ }();^.",
+		question_completed: false,
+		needs_review: true,
+		question_id: 10
+		}
+	]
+
+const TestData = [
+	{	name: 'dummy test 1',
+		date_taken: dummyDate,
+		time_total: 30000,
+		time_taken: 20000,
+		questions: dummyQuestions
+	},
+	{	name: 'dummy test 2',
+		date_taken: dummyDate,
+		time_total: 40000,
+		time_taken: 20000,
+		questions: dummyQuestions
+	},
+	{	name: 'dummy test 3',
+		date_taken: dummyDate,
+		time_total: 25000,
+		time_taken: 20000,
+		questions: dummyQuestions
+	}
+];
+
+TestData.forEach(test => {
+	TestTable.storeTest(test)
+		.then(testId => console.log('testId', testId))
+		.catch(err => console.error('error', err));
+})
 
 console.log('database seeded');
