3386627c32716ca952aac258eec262f0181cec15
Carol Chung <cch5ng@gmail.com>
2019-01-27 23:50:14 -0800

    start setup to use redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import ReduxThunk from 'redux-thunk'
+import App from './containers/App'
+import appStore from './store/index.js'
+//import './index.css'
+//import { render } from 'react-dom';
 
-render(
-	<div>My app</div>,
-	document.getElementById('root')
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(appStore,
+  composeEnhancers(
+    applyMiddleware(ReduxThunk)
+  )
+)
+
+ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
+
+// render(
+// 	<div>My app</div>,
+// 	document.getElementById('root')
+// );
