1b67122fb45d0212ecdce6c1fb3c3f2a8a65f2a4
Carol Chung <cch5ng@gmail.com>
2019-01-28 12:36:41 -0800

    add helper logic to format display data better

--- client/src/containers/tests/Tests.js
+++ client/src/containers/tests/Tests.js
@@ -19,19 +19,41 @@ class Tests extends Component {
 	}
 
 	getQuestionsMissedCount(questionsAr) {
+		let missedCount = 0;
+		
+		questionsAr.forEach(question => {
+			if (!question.question_completed) {
+				missedCount += 1;
+			}
+		});
 
+		return missedCount;
 	}
 
 	getPrettyTime(timeMs) {
+		let hour;
+		let minute = Math.floor(timeMs / 60000);
+		let second = Math.floor(timeMs / 1000);
+		let prettyTimeStr = `${minute} minutes, ${second} seconds`;
 
+
+		console.log('prettyTimeStr', prettyTimeStr);
+		return prettyTimeStr;
 	}
 
 	getPrettyDate(date) {
 
 	}
 
-	getAvgTimePerQuestion(totalTime, missedQuestCount) {
+	getAvgTimePerQuestion(totalTime, completedQuestCount) {
 
+		if (completedQuestCount === 0) {
+			return 0;
+		}
+
+		let avgTime = totalTime / completedQuestCount;
+
+		return this.getPrettyTime(avgTime);
 	}
 
 	render() {
@@ -43,25 +65,30 @@ class Tests extends Component {
 			testsAr = Object.keys(tests.tests).map(testKey => {
 				return this.props.tests.tests[testKey];
 			})
-
 		}
 
 		return (
 			<div>
 				<h2>All Tests</h2>
 
-				{testsAr.map(test => (
-					<div className="test" key={test.id} >
-						<ul>
-							<li>Name {test.name}</li>
-							<li>Date {test.date_taken}</li>
-							<li>{test.questions ? test.questions.length : null} questions</li>
-							<li>ZZ questions missed</li>
-							<li>Total Time {test.time_total}</li>
-							<li>Time per question ZZ</li>
-						</ul>
-					</div>
-				))}
+				{testsAr.map(test => {
+					const missedQuestions = test.questions ? this.getQuestionsMissedCount(test.questions) : null;
+					const completedQuestionsCount = test.questions ? test.questions.length - missedQuestions : null;
+
+
+					return (
+						<div className="test" key={test.id} >
+							<ul>
+								<li>Name {test.name}</li>
+								<li>Date {test.date_taken}</li>
+								<li>{test.questions ? test.questions.length : null} questions</li>
+								<li>{missedQuestions} questions missed</li>
+								<li>Total Time {test.time_total}</li>
+								<li>Time per question {test.questions ? this.getAvgTimePerQuestion(test.time_total, completedQuestionsCount): null}</li>
+							</ul>
+						</div>
+					)	
+				})}
 			</div>
 		)
 	}
