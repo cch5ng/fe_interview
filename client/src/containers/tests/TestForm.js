02338b8eed01726d15a84a270c068ff23e962a7c
Carol Chung <cch5ng@gmail.com>
2019-01-28 21:53:03 -0800

    add logic to handle checkbox interaction; stub out next steps on form submit handler; need to do a bit more

--- client/src/containers/tests/TestForm.js
+++ client/src/containers/tests/TestForm.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 //import { fetchTests } from './TestActions';
 
+const questionCategories = [
+	`Coding Questions`,
+	`CSS Questions`,
+	`Fun Questions`,
+	`General Questions`,
+	`HTML Questions`,
+	`JavaScript Questions`,
+	`Network Questions`,
+	`Performance Questions`,
+	`Testing Questions`
+];
+
+const initState = {
+	inputTestName: '',
+	inputQuestionCount: 0,
+	selectTimePerQuestion: 'no limit',
+	CodingQuestions: false,
+	CSSQuestions: false,
+	FunQuestions: false,
+	GeneralQuestions: false,
+	HTMLQuestions: false,
+	JavaScriptQuestions: false,
+	NetworkQuestions: false,
+	PerformanceQuestions: false,
+	TestingQuestions: false
+}
+
 class TestForm extends Component {
 
 	constructor(props) {
@@ -10,10 +37,22 @@ class TestForm extends Component {
 		this.state = {
 			inputTestName: '',
 			inputQuestionCount: 0,
-			selectTimePerQuestion: 'no limit'
+			selectTimePerQuestion: 'no limit',
+			CodingQuestions: false,
+			CSSQuestions: false,
+			FunQuestions: false,
+			GeneralQuestions: false,
+			HTMLQuestions: false,
+			JavaScriptQuestions: false,
+			NetworkQuestions: false,
+			PerformanceQuestions: false,
+			TestingQuestions: false
 		}
 
 		this.handleInputChange = this.handleInputChange.bind(this);
+		this.resetState = this.resetState.bind(this);
+		this.handleFormSubmit = this.handleFormSubmit.bind(this);
+		this.generateTest = this.generateTest.bind(this);
 	}
 
 	//event handlers
@@ -29,6 +68,26 @@ class TestForm extends Component {
 	}
 
 	// form submit handler
+	handleFormSubmit(ev) {
+		ev.preventDefault();
+
+		// do stuff, call action
+		// save initial data to redux
+		// do calculations to generate a pseudo random test
+		// redirect to a new view (summary view to be able to start the test)
+
+		this.resetState();
+	}
+
+	// reset state
+	resetState(ev) {
+		ev.preventDefault();
+		this.setState(initState);
+	}
+
+	generateTest(numQuestions, categoriesAr, questions) {
+
+	}
 
 	render() {
 		return (
@@ -71,16 +130,28 @@ class TestForm extends Component {
 					<div>
 						<h2>Question Categories</h2>
 
-						<input type="checkbox" name="checkbox-question-category"></input>
-						<label></label>
-						<input type="checkbox" name="checkbox-question-category"></input>
-						<label></label>
+						{questionCategories.map(category => {
+							let categoryAr = category.split(' ');
+							const categoryShort = categoryAr[0];
+							const categoryConcat = categoryAr.join('');
+
+							return (
+								<React.Fragment key={categoryConcat}>
+									<input type="checkbox" name={categoryConcat} checked={this.state[categoryConcat]} 
+										onChange={this.handleInputChange}
+									/>
+									<label>{categoryShort}</label>
+								</React.Fragment>
+							)
+						})}
+
 					</div>
 
-					<button id="button-create-test">Create</button>
-
-					<button id="button-cancel-test">Cancel</button>
-
+					<button id="button-create-test" onClick={handleFormSubmit}>Create</button>
+					
+					{/* maybe more confusing than useful
+					<button id="button-cancel-test" onClick={this.resetState} >Cancel</button>
+					*/}
 
 				</form>
 			</div>
