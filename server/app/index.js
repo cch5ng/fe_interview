74143e8d8d4b483591d35de58d11adf365900de9
Carol Chung <cch5ng@gmail.com>
2019-01-29 09:22:40 -0800

    add BE endpoint to get all questions

--- server/app/index.js
+++ server/app/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const testRouter = require('./api/test');
+const questionRouter = require('./api/question');
 
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:1234' }));
 app.use(bodyParser.json());
 app.use('/test', testRouter);
+app.use('/question', questionRouter);
 
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
