9349e968040b8ddec7a057d1c977696b3000ea34
Carol Chung <cch5ng@gmail.com>
2019-01-28 16:10:52 -0800

    clean up tests for helper

--- client/utils/helper.test.js
+++ client/utils/helper.test.js
@@ -4,19 +4,20 @@ describe('helper getPrettyTime()', () => {
 
 	it('should convert minutes', () => {
 		const timeMs = 1800000;
-		expect(getPrettyTime(timeMs)).toEqual(`30 minutes, 0 seconds`);
+		const timeStr = `30 minutes`;
+		expect(getPrettyTime(timeMs)).toEqual(timeStr);
 	});
 
-	// it('should convert hours/minutes', () => {
+	it('should convert hours/minutes', () => {
+		const timeMs2 = 4800000;
+		const timeStr2 = `1 hour, 20 minutes`;
+		expect(getPrettyTime(timeMs2)).toEqual(timeStr2);
+	});
 
-	// });
-
-	// it('should convert even hours/minutes/seconds', () => {
-
-	// });
-
-	// it('should convert even hours/minutes/seconds', () => {
-
-	// });
+	it('should convert hours', () => {
+		const timeMs3 = 7200000;
+		const timeStr3 = `2 hours`;
+		expect(getPrettyTime(timeMs3)).toEqual(timeStr3);
+	});
 
 });
