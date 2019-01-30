e35b62e67390b1c344aa6772a792376d0022a600
Carol Chung <cch5ng@gmail.com>
2019-01-30 13:38:45 -0800

    work out further logic to fetchRandomTest(); still working

--- client/src/containers/tests/TestActions.js
+++ client/src/containers/tests/TestActions.js
@@ -1,6 +1,8 @@
+import { getRandomlyOrderedList } from '../../../utils/helper';
+
 // fetch constants
 const API_GET_TESTS = 'http://localhost:3000/test/all';
-const API_POST_RANDOM_TEST = 'http://localhost:3000/test/random';
+const API_POST_RANDOM_TEST = 'http://localhost:3000/question/random';
 
 // action types
 export const REQUEST_ALL_TESTS = 'REQUEST_ALL_TESTS';
@@ -40,25 +42,51 @@ export function requestRandomTest() {
 	}
 }
 
-export function receiveRandomTest(test) {
+export function receiveRandomTest(curTest) {
 	return {
 		type: RECEIVE_RANDOM_TEST,
-		tests,
+		curTest,
 		retrieving: false
 	}
 }
 
-export const fetchRandomTest = (data) => dispatch => {
+export const fetchRandomTest = (questionData, testData) => dispatch => {
 	dispatch(requestRandomTest());
 	return fetch(API_POST_RANDOM_TEST,
 			{	method: 'POST',
 				headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(questionData),
 			}
 		)
 		.then(resp => resp.json())
-		.then(json => dispatch(receiveRandomTest(json)))
+		.then(json => {
+			let flatQuestionsAr = [];
+			let curTestObj = {};
+			let randomizedQuestions = [];
+			// concat lists of questions into one flat list
+
+			json.forEach(questList => {
+				questList.forEach(quest => {
+					flatQuestionsAr.push(quest);
+				})
+			})
+			console.log('flatQuestionsAr', flatQuestionsAr);
+
+			// generate a randomized order of the flat list
+			randomizedQuestions = getRandomlyOrderedList(flatQuestionsAr);
+			console.log('randomizedQuestions', randomizedQuestions);
+
+			curTestObj.name = testData.name;
+			curTestObj.questions = randomizedQuestions;
+			curTestObj.date_taken = null;
+			curTestObj.time_total = testData.time_total;
+
+
+			// make a whole current test object
+			dispatch(receiveRandomTest(json))
+			//dispatch async action to create a test in backend
+		})
 		.catch(err => console.error('fetch error', err));
-} 
+}
