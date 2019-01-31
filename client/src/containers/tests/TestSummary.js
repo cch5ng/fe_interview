71322f2e5dddf2b1f4507a7969c25a465af03289
Carol Chung <cch5ng@gmail.com>
2019-01-31 14:25:13 -0800

    update test summary view

--- client/src/containers/tests/TestSummary.js
+++ client/src/containers/tests/TestSummary.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-const initState = {
-}
-
 class TestSummary extends Component {
 
 	constructor(props) {
 		super(props);
 
-		this.state = initState;
+		this.state = {
+			//status: initialized,
+		};
 
 		//this.getRandomArbitrary = this.getRandomArbitrary.bind(this);
 	}
@@ -22,6 +21,7 @@ class TestSummary extends Component {
 
 	render() {
 		let curTestObj = this.props.tests && this.props.tests.curTest ? this.props.tests.curTest : null;
+		let status = curTestObj && curTestObj.status ? curTestObj.status : 'initialized';
 		// let questionsAr = [];
 		// let questionsMaxObj = {};
 
@@ -38,10 +38,20 @@ class TestSummary extends Component {
 					<div>
 						<ul>
 							<li>Name {curTestObj.name}</li>
+							<li>Total time {curTestObj.time_total}</li>
 							<li>Num Questions {curTestObj.questions.length}</li>
 						</ul>
 					</div>
 				)}
+
+				{status === 'initialized' && (
+					<button>Start</button>
+				)}
+
+				{status === 'active' && (
+					<button>Submit</button>
+				)}
+
 			</div>
 		)
 	}
