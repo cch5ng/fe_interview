7a786aefba7b8fd6dfa6c61eea21c39aa51312b7
Carol Chung <cch5ng@gmail.com>
2019-02-04 14:32:48 -0800

    add BE route to handle update to question during test

--- server/app/api/test.js
+++ server/app/api/test.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const TestTable = require('../test/table');
+const TestQuestionTable = require('../testQuestion/table');
 
 const router = Router();
 
@@ -20,5 +21,14 @@ router.post('/new', (req, res, next) => {
 		.then(testId => res.json(testId))
 		.catch(err => console.error('error', err))
 })
+
+router.post('/updateQuestion', (req, res, next) => {
+	console.log('req.body', req.body);
+	TestQuestionTable.updateTestQuestion(req.body)
+		//.then(() => console.log('question updated in test_question table'))
+		.then(questionId => res.json(questionId))
+		.catch(err => console.error('error', err))
+})
+
 
 module.exports = router;
