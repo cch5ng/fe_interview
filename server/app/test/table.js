26168283c873f92e9945c21984db31390eeaf884
Carol Chung <cch5ng@gmail.com>
2019-02-05 15:23:52 -0800

    refactor for test table col change

--- server/app/test/table.js
+++ server/app/test/table.js
@@ -5,17 +5,17 @@ const QuestionTable = require ('../question/table');
 class TestTable {
 
 	static storeTest(test) {
-		const { name, date_taken, time_total, time_taken, questions, status } = test; //user_id
+		const { name, date_taken, time_total, time_remaining, questions, status } = test; //user_id
 
 		return new Promise((resolve, reject) => {
 			// QuestionTable.getIdFromContent({ content })
 				//.then(({ id }) => {
 
 			pool.query(
-				`INSERT INTO test(name, date_taken, time_total, time_taken, status)
+				`INSERT INTO test(name, date_taken, time_total, time_remaining, status)
 					VALUES($1, $2, $3, $4, $5)
 					RETURNING id`,
-				[name, date_taken, time_total, time_taken, status],
+				[name, date_taken, time_total, time_remaining, status],
 				(err, resp) => {
 					if (err) return reject(err);
 
@@ -36,29 +36,47 @@ class TestTable {
 
 	static getAllTests() {
 		return new Promise((resolve, reject) => {
-				pool.query(
-					`SELECT test.id, time_total, time_taken, name, date_taken, status from test`,
-					[],
-					(err, resp) => {
-						if (err) return reject(err);
+			pool.query(
+				`SELECT test.id, time_total, time_taken, name, date_taken, status from test`,
+				[],
+				(err, resp) => {
+					if (err) return reject(err);
 
-						const tests = resp.rows;
+					const tests = resp.rows;
 
-						tests.forEach((test, idx) => {
-							console.log('test', test);
-							TestQuestionTable.getQuestionsByTestId({ test_id: test.id })
-								.then(questions => {
-									test.questions = questions;
-									if (idx === tests.length - 1) {
-										resolve(tests);
-									}
-								})
-								.catch(err => console.error('error', err))
-						})
+					tests.forEach((test, idx) => {
+						console.log('test', test);
+						TestQuestionTable.getQuestionsByTestId({ test_id: test.id })
+							.then(questions => {
+								test.questions = questions;
+								if (idx === tests.length - 1) {
+									resolve(tests);
+								}
+							})
+							.catch(err => console.error('error', err))
+					})
 
-					}
-				)
-			})
+				}
+			)
+		})
+	}
+
+	static updateTest({time_remaining, status, test_id}) {
+		return new Promise((resolve, reject) => {
+			pool.query(
+				`UPDATE test
+					SET time_remaining = $1, status = $2
+					WHERE id = $3
+					RETURNING id
+				`,
+				[time_remaining, status, test_id],
+				(err, resp) => {
+					if (err) return reject(err);
+
+					resolve(resp.rows[0].id);
+				}
+			)
+		})
 	}
 
 }
