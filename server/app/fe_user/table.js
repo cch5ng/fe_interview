213830d5c7f2636fd25c4d39080e861c0b2ffa36
Carol Chung <cch5ng@gmail.com>
2019-02-08 16:25:13 -0800

    add functions to store user and find user; add user to seed data

--- server/app/fe_user/table.js
+++ server/app/fe_user/table.js
@@ -2,23 +2,47 @@ const pool = require('../../databasePool');
 
 class FEUserTable {
 
-	static findByUserName(username) {
+	static storeFEUser(user) {
+		const { email, password} = user;
+		return new Promise((resolve, reject) => {
+			pool.query(
+				`INSERT INTO fe_user(email, password)
+					VALUES($1, $2)
+					RETURNING id
+				`,
+				[email, password],
+				(err, resp) => {
+					if (err) return reject(err);
+
+					const userId = resp.rows[0].id;
+					resolve({ userId });
+				}
+			)
+		})
+
+	}
+
+	static findByUserName(email) {
 		return new Promise((resolve, reject) => {
 			pool.query(
 				`SELECT id
 					FROM fe_user
-					WHERE user_name = $1`,
-				[username],
+					WHERE email = $1`,
+				[email],
 				(err, resp) => {
 					if (err) return reject(err);
 
+					// TODO handle when user_id returns no results
 					const user_id = resp.rows[0];
 					resolve(user_id);
 				}
 			)
 		})
 	}
-
 }
+
+// test FEUserTable
+
+FEUserTable.findByUserName()
 
 module.exports = FEUserTable;
