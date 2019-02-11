c2cd26341572168819f7e53a87d095cf63bcd96a
Carol Chung <cch5ng@gmail.com>
2019-02-10 22:02:57 -0800

    update fe_user table to make email values unique

--- server/bin/sql/fe_user.sql
+++ server/bin/sql/fe_user.sql
@@ -1,6 +1,6 @@
 CREATE TABLE fe_user(
 	id					SERIAL PRIMARY KEY,
-	email 			VARCHAR(64),
+	email 			VARCHAR(64) UNIQUE,
 	password 		VARCHAR(64),
 	user_name 	VARCHAR(64),
 	created_at 	TIMESTAMP NOT NULL DEFAULT NOW()
