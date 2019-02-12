2efa7ffd037fc28e81818491c92ebaac6c8c3c6c
Carol Chung <cch5ng@gmail.com>
2019-02-12 10:58:45 -0800

    stub out logic to handle BE login endpoint; missing jwt construction

--- server/app/api/auth.js
+++ server/app/api/auth.js
@@ -20,16 +20,21 @@ router.post('/register', (req, res, next) => {
 		})
 })
 
-// router.post('/login', 
-// 	passport.authenticate('local'),
-// 	(req, res, next) => {
-// 		if (res) {console.log('res', res)}
-
-// 	// XXX
-// 	// 	.then(result => {
-// 	// 	})
-// 	// 	.catch(err => console.error('error', err))
-// })
+router.post('/login', (req, res, next) => {
+	let {email, password} = req.body;
+	FEUserTable.findByEmail({email, password})	
+		.then(result => {
+			console.log('login result', result)
+			if (result.error) {
+				res.json(result);
+			}
+			if (result.userId) {
+				// here create the jwt and return in
+				res.json({jwt: 'todo'});
+			}
+		})
+		.catch(err => console.error('error', err))
+})
 
 
 
