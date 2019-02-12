37a658f99e5d46629ce0f68076a500a6ab866600
Carol Chung <cch5ng@gmail.com>
2019-02-12 10:46:31 -0800

    comment out passport temp while trying to figure it out; probably go with jwt strategy, remove local

--- server/app/index.js
+++ server/app/index.js
@@ -2,35 +2,40 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const testRouter = require('./api/test');
 const questionRouter = require('./api/question');
 const authRouter = require('./api/auth');
 const FEUserTable = require('./fe_user/table');
 
 // passport auth configure
-
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new LocalStrategy(
-  function(username, password, cb) {
-    FEUserTable.findByEmail({email: username, password})
-    	.then(userId => {
-    		if (!userId) {return cb(null, false);}
-    		return cb(null, userId);
-    	})
-    	.catch(err => cb(err))
-  }));
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+//TODO store secret, figure out other options
+//opts.secretOrKey = 'secret';
+//opts.issuer = 'accounts.examplesoft.com';
+//opts.audience = 'yoursite.net';
+//passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    //TODO how to verify the jwt for the user
+    // User.findOne({id: jwt_payload.sub}, function(err, user) {
+    //     if (err) {
+    //         return done(err, false);
+    //     }
+    //     if (user) {
+    //         return done(null, user);
+    //     } else {
+    //         return done(null, false);
+    //         // or you could create a new account
+    //     }
+    // });
+//}));
 
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:1234' }));
 app.use(bodyParser.json());
-app.use(passport.initialize());
+//app.use(passport.initialize());
 app.use('/test', testRouter);
 app.use('/question', questionRouter);
 app.use('/auth', authRouter);
