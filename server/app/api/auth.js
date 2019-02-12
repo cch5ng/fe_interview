91c667857e691fc2c74fbc81c319ee00177b05df
Carol Chung <cch5ng@gmail.com>
2019-02-12 10:45:51 -0800

    update logic for handling register route on BE

--- server/app/api/auth.js
+++ server/app/api/auth.js
@@ -4,16 +4,32 @@ const FEUserTable = require('../fe_user/table');
 
 const router = Router();
 
-router.post('/login', 
-	passport.authenticate('local'),
-	(req, res, next) => {
-		if (res) {console.log('res', res)}
 
-	// XXX
-	// 	.then(result => {
-	// 	})
-	// 	.catch(err => console.error('error', err))
+router.post('/register', (req, res, next) => {
+	let {email, password} = req.body;
+	FEUserTable.storeUser({email, password})
+  	.then(userId => { 
+  		res.json(userId);
+  	})
+		.catch(err => {
+			let message;
+			if (err.detail.indexOf('already exists') > -1) {
+				message = 'user already exists';
+			}
+			res.json({error: message});
+		})
 })
+
+// router.post('/login', 
+// 	passport.authenticate('local'),
+// 	(req, res, next) => {
+// 		if (res) {console.log('res', res)}
+
+// 	// XXX
+// 	// 	.then(result => {
+// 	// 	})
+// 	// 	.catch(err => console.error('error', err))
+// })
 
 
 
