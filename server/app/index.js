f0b413c2e6c638d32c2fe1307e28e66bfa5861a5
Carol Chung <cch5ng@gmail.com>
2019-02-12 11:22:53 -0800

    add env var file

--- server/app/index.js
+++ server/app/index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
