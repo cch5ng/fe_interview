010ea653fcf973f4f1987aa2880dab3780866f58
Carol Chung <cch5ng@gmail.com>
2019-02-13 09:55:23 -0800

    add passport jwt strategy for test endpoints

--- server/app/api/test.js
+++ server/app/api/test.js
@@ -7,7 +7,6 @@ const router = Router();
 
 router.post('/all',
 	passport.authenticate('jwt', { session: false }),
-
 	(req, res, next) => {
 		TestTable.getAllTests()
 			.then(tests => {
@@ -19,34 +18,42 @@ router.post('/all',
 			});
 });
 
-router.post('/detail', (req, res, next) => {
-	TestTable.getTestById(req.body)
-		.then(result => {
-			let newTest = result[0];
-			let testQuestions = result[1];
-			newTest.questions = testQuestions;
-			res.json(newTest);
-		})
-		.catch(err => console.error('error', err))
+router.post('/detail',
+	passport.authenticate('jwt', { session: false }),
+	(req, res, next) => {
+		TestTable.getTestById(req.body)
+			.then(result => {
+				let newTest = result[0];
+				let testQuestions = result[1];
+				newTest.questions = testQuestions;
+				res.json(newTest);
+			})
+			.catch(err => {
+				//console.error('error', err)
+				next(err);
+			})
 })
 
-router.post('/new', (req, res, next) => {
-	console.log('req.body', req.body);
-	TestTable.storeTest(req.body)
-		.then(testId => res.json(testId))
-		.catch(err => console.error('error', err))
+router.post('/new', 
+	passport.authenticate('jwt', { session: false }),
+	(req, res, next) => {
+		TestTable.storeTest(req.body)
+			.then(testId => res.json(testId))
+			.catch(err => console.error('error', err))
 })
 
-router.post('/update', (req, res, next) => {
-	console.log('req.body', req.body);
-	TestTable.updateTest(req.body)
-		//.then(() => console.log('question updated in test_question table'))
-		.then(testId => res.json(testId))
-		.catch(err => console.error('error', err))
+router.post('/update',
+	passport.authenticate('jwt', { session: false }),
+	(req, res, next) => {
+		TestTable.updateTest(req.body)
+			//.then(() => console.log('question updated in test_question table'))
+			.then(testId => res.json(testId))
+			.catch(err => console.error('error', err))
 })
 
-router.post('/updateQuestion', (req, res, next) => {
-	console.log('req.body', req.body);
+router.post('/updateQuestion',
+	passport.authenticate('jwt', { session: false }),
+	(req, res, next) => {
 	TestQuestionTable.updateTestQuestion(req.body)
 		//.then(() => console.log('question updated in test_question table'))
 		.then(questionId => res.json(questionId))
