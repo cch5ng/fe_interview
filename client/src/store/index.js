a60382f24a0ffb43351ffb4ebd2e0a7adba1567f
Carol Chung <cch5ng@gmail.com>
2019-02-12 16:32:52 -0800

    add auth to store for register and login

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { tests } from '../containers/tests/TestReducers';
 import { questions } from '../containers/questions/QuestionReducers';
+import { auth } from '../containers/auth/AuthReducers';
 
 export default combineReducers({
   tests,
-  questions
+  questions,
+  auth,
 });
