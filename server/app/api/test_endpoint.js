6a6adc59e7ffb83eedcdb6ad1b392dd09fee7193
Carol Chung <cch5ng@gmail.com>
2019-02-17 15:29:59 -0800

    rename test as test_endpoint to avoid jest file name conflict

--- server/app/api/test_endpoint.js
+++ server/app/api/test_endpoint.js
@@ -1,3 +1,5 @@
+// renamed so it would not conflict with jest test file naming
+
 const { Router } = require('express');
 const passport = require('passport');
 const TestTable = require('../test/table');
