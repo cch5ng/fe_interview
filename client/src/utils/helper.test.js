19e877ae09a8f5a1d279ce756efabf2e2e95b2de
Carol Chung <cch5ng@gmail.com>
2019-02-18 15:06:14 -0800

    update string formatting for time

--- client/src/utils/helper.test.js
+++ client/src/utils/helper.test.js
@@ -4,26 +4,25 @@ describe('helper getPrettyTime()', () => {
 
 	it('should convert minutes', () => {
 		const timeMs = 1800000;
-		const timeStr = `30 minutes`;
+		const timeStr = `30 min`;
 		expect(getPrettyTime(timeMs)).toEqual(timeStr);
 	});
 
 	it('should convert hours/minutes', () => {
 		const timeMs2 = 4800000;
-		const timeStr2 = `1 hour, 20 minutes`;
+		const timeStr2 = `1 hr, 20 min`;
 		expect(getPrettyTime(timeMs2)).toEqual(timeStr2);
 	});
 
 	it('should convert hours', () => {
 		const timeMs3 = 7200000;
-		const timeStr3 = `2 hours`;
+		const timeStr3 = `2 hr`;
 		expect(getPrettyTime(timeMs3)).toEqual(timeStr3);
 	});
 
 });
 
 // TODO set of tests for getPrettyDate()
-
 
 describe('helper getRandomlyOrderedList()', () => {
 
