534a5564575708ce0c5827b7996a65f51253e564
Carol Chung <cch5ng@gmail.com>
2019-02-19 14:21:20 -0800

    stub out mock for getRandomQuestions()

--- client/src/utils/__mocks__/http_requests.js
+++ client/src/utils/__mocks__/http_requests.js
@@ -16,6 +16,44 @@ const questionsData = [
 	}
 ];
 
+const randomQuestionsData = [
+	{
+		category: "CSS Questions",
+		child_content: null,
+		content: "dummy question 59",
+		id: 59,
+		sort_order: 0
+	},
+	{
+		category: "CSS Questions",
+		child_content: null,
+		content: "dummy question 57",
+		id: 57,
+		sort_order: 1
+	},
+	{
+		category: "CSS Questions",
+		child_content: null,
+		content: "What is CSS selector specificity and how does it work?",
+		id: 55,
+		sort_order: 2
+	},
+	{
+		category: "CSS Questions",
+		child_content: null,
+		content: "What's the difference between 'resetting' and 'normalizing' CSS? Which would you choose, and why?",
+		id: 56,
+		sort_order: 3
+	},
+	{
+		category: "CSS Questions",
+		child_content: null,
+		content: "dummy question 58",
+		id: 58,
+		sort_order: 4
+	}
+];
+
 const Questions = {
 	getAll: (p) => {
 		return new Promise((resolve, reject) => {
@@ -25,6 +63,15 @@ const Questions = {
 
 			resolve(questionsData);
 		})
+	},
+	getRandomQuestions: (questionData) => {
+		return new Promise((resolve, reject) => {
+			if (!questionData) {
+				reject(new Error('random questions could not be retrieved'));
+			}
+
+			resolve(randomQuestionsData);
+		})		
 	}
 }
 
