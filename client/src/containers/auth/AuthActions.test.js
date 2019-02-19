0b398940da3337b34b285a0a69bf67496626d05f
Carol Chung <cch5ng@gmail.com>
2019-02-19 14:20:32 -0800

    reorganize for clarity

--- client/src/containers/auth/AuthActions.test.js
+++ client/src/containers/auth/AuthActions.test.js
@@ -10,72 +10,77 @@ const mockStore = configureMockStore(middlewares);
 
 describe('Async Auth Actions', () => {
 
-	const expectedActions = [
-    { type: REQUEST_REGISTRATION, retrieving: true },
-    { type: RECEIVE_REGISTRATION, retrieving: false,
-			userRegistered: true,
-			registrationError: '',
-		}
-  ];
+	describe('fetch register ', () => {
+		const expectedActions = [
+	    { type: REQUEST_REGISTRATION, retrieving: true },
+	    { type: RECEIVE_REGISTRATION, retrieving: false,
+				userRegistered: true,
+				registrationError: '',
+			}
+	  ];
 
-  const registration = {
-  	email: 'dummy@d.com',
-  	password: 'password'
-  }
+	  const registration = {
+	  	email: 'dummy@d.com',
+	  	password: 'password'
+	  }
 
-	const expectedActionsFail = [
-    { type: REQUEST_REGISTRATION, retrieving: true },
-    { type: RECEIVE_REGISTRATION, retrieving: false,
-			userRegistered: false,
-			registrationError: 'registration could not be completed',
-		}
-  ];
+		const expectedActionsFail = [
+	    { type: REQUEST_REGISTRATION, retrieving: true },
+	    { type: RECEIVE_REGISTRATION, retrieving: false,
+				userRegistered: false,
+				registrationError: 'registration could not be completed',
+			}
+	  ];
 
-	it('fetch register should return', () => {
-		const store = mockStore({ auth: {} })
-    return store.dispatch(fetchRegister(registration)).then(() => {
-      expect(store.getActions()).toEqual(expectedActions)
-    })
+		it('fetch register should return', () => {
+			const store = mockStore({ auth: {} })
+	    return store.dispatch(fetchRegister(registration)).then(() => {
+	      expect(store.getActions()).toEqual(expectedActions)
+	    })
+		})
+
+		it('fetch register should handle error', () => {
+			const store = mockStore({ auth: {} })
+	    return store.dispatch(fetchRegister()).then(() => {
+	      expect(store.getActions()).toEqual(expectedActionsFail)
+	    })
+		})
 	})
 
-	it('fetch register should handle error', () => {
-		const store = mockStore({ auth: {} })
-    return store.dispatch(fetchRegister()).then(() => {
-      expect(store.getActions()).toEqual(expectedActionsFail)
-    })
+	describe('fetch login ', () => {
+		const expectedLoginActions = [
+	    { type: REQUEST_LOGIN, retrieving: true },
+	    { type: RECEIVE_LOGIN, retrieving: false,
+				jwt: 'dummy token',
+				email: 'zzz@z.com',
+				loginError: ''
+			}
+	  ];
+
+		it('fetch login should return', () => {
+			const store = mockStore({ auth: {} })
+	    return store.dispatch(fetchLogin(registration)).then(() => {
+	      expect(store.getActions()).toEqual(expectedLoginActions)
+	    })
+		})
+
+		const expectedLoginActionsFail = [
+	    { type: REQUEST_LOGIN, retrieving: true },
+	    { type: RECEIVE_LOGIN, retrieving: false,
+				jwt: '',
+				email: null,
+				loginError: 'login could not be completed'
+			}
+	  ];
+
+		it('fetch login should handle error', () => {
+			const store = mockStore({ auth: {} })
+	    return store.dispatch(fetchLogin()).then(() => {
+	      expect(store.getActions()).toEqual(expectedLoginActionsFail)
+	    })
+		})
 	})
 
-	const expectedLoginActions = [
-    { type: REQUEST_LOGIN, retrieving: true },
-    { type: RECEIVE_LOGIN, retrieving: false,
-			jwt: 'dummy token',
-			email: 'zzz@z.com',
-			loginError: ''
-		}
-  ];
-
-	it('fetch login should return', () => {
-		const store = mockStore({ auth: {} })
-    return store.dispatch(fetchLogin(registration)).then(() => {
-      expect(store.getActions()).toEqual(expectedLoginActions)
-    })
-	})
-
-	const expectedLoginActionsFail = [
-    { type: REQUEST_LOGIN, retrieving: true },
-    { type: RECEIVE_LOGIN, retrieving: false,
-			jwt: '',
-			email: null,
-			loginError: 'login could not be completed'
-		}
-  ];
-
-	it('fetch login should handle error', () => {
-		const store = mockStore({ auth: {} })
-    return store.dispatch(fetchLogin()).then(() => {
-      expect(store.getActions()).toEqual(expectedLoginActionsFail)
-    })
-	})
 
 
 })
