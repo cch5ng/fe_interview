d77f3eedb888ceabcdb1908d4be73cdae7982309
Carol Chung <cch5ng@gmail.com>
2019-02-20 15:29:37 -0800

    stub out some logic for production, should get db connection info from env vars, not secrets file

--- server/databasePool.js
+++ server/databasePool.js
@@ -1,11 +1,22 @@
 const { Pool } = require('pg');
-const databaseConfiguration = {
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_DATABASE,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
-}
+let databaseConfiguration = {}
+
+// TODO this section does not test locally well b/c when code executes, env var have not been loaded
+// must test on production
+
+//if (process.env.NODE_ENV === 'production') {
+
+// 	databaseConfiguration = {	user: process.env.DB_USER,
+// 		host: process.env.DB_HOST,
+// 		database: process.env.DB_DATABASE,
+// 		password: process.env.DB_PASSWORD,
+// 		port: process.env.DB_PORT,
+// 	}
+
+// 	console.log('db', databaseConfiguration)
+// } else {
+	databaseConfiguration = require('./secrets/databaseConfiguration');
+//}
 
 const pool = new Pool(databaseConfiguration);
 
