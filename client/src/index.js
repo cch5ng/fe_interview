245b4d2de5ddfeae66d5650721238837474af950
Carol Chung <cch5ng@gmail.com>
2019-02-19 22:41:33 -0800

    cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import ReduxThunk from 'redux-thunk'
-import App from './containers/App'
-import appStore from './store/index.js'
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import App from './containers/App';
+import appStore from './store/index.js';
 //import './index.css'
 //import { render } from 'react-dom';
 
