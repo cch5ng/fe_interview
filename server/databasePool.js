8103040791e7a7d85d10351402c7129f7562d994
Carol Chung <cch5ng@gmail.com>
2019-02-20 19:18:03 -0800

    update db config to handle connect str when deployed

--- server/databasePool.js
+++ server/databasePool.js
@@ -1,22 +1,26 @@
 const { Pool } = require('pg');
 let databaseConfiguration = {}
+let connectionString;
+let pool;
 
 // TODO this section does not test locally well b/c when code executes, env var have not been loaded
 // must test on production
 
 if (process.env.NODE_ENV === 'production') {
 
-	databaseConfiguration = {	user: process.env.DB_USER,
-		host: process.env.DB_HOST,
-		database: process.env.DB_DATABASE,
-		password: process.env.DB_PASSWORD,
-		port: process.env.DB_PORT,
-	}
+	connectionString = process.env.DATABASE_URL;
+	pool = new Pool({ connectionString });
+
+	// databaseConfiguration = {	user: process.env.DB_USER,
+	// 	host: process.env.DB_HOST,
+	// 	database: process.env.DB_DATABASE,
+	// 	password: process.env.DB_PASSWORD,
+	// 	port: process.env.DB_PORT,
+	// }
 
 } else {
 	databaseConfiguration = require('./secrets/databaseConfiguration');
+	pool = new Pool(databaseConfiguration);
 }
-
-const pool = new Pool(databaseConfiguration);
 
 module.exports = pool;
