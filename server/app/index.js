343f4ec8c223e9d2265bf7d03ed9664dba93140e
Carol Chung <carol.chung@submittable.com>
2019-02-21 11:56:07 -0800

    89 deploy fix (#90)
    * try moving dist under server/app
    
    * try moving dist under server/app
    
    * try moving dist under server/app

--- server/app/index.js
+++ server/app/index.js
@@ -72,9 +72,9 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../dist')));
+  app.use(express.static(path.join(__dirname, '/dist')));
   app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'../dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
   });
 }
 
