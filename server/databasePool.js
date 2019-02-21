9622837ca1ca1c274c5f59c9018cca2e421b067a
Carol Chung <cch5ng@gmail.com>
2019-02-20 18:20:10 -0800

    get db config from env var for deploy

--- server/databasePool.js
+++ server/databasePool.js
@@ -4,19 +4,18 @@ let databaseConfiguration = {}
 // TODO this section does not test locally well b/c when code executes, env var have not been loaded
 // must test on production
 
-//if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 
-// 	databaseConfiguration = {	user: process.env.DB_USER,
-// 		host: process.env.DB_HOST,
-// 		database: process.env.DB_DATABASE,
-// 		password: process.env.DB_PASSWORD,
-// 		port: process.env.DB_PORT,
-// 	}
+	databaseConfiguration = {	user: process.env.DB_USER,
+		host: process.env.DB_HOST,
+		database: process.env.DB_DATABASE,
+		password: process.env.DB_PASSWORD,
+		port: process.env.DB_PORT,
+	}
 
-// 	console.log('db', databaseConfiguration)
-// } else {
+} else {
 	databaseConfiguration = require('./secrets/databaseConfiguration');
-//}
+}
 
 const pool = new Pool(databaseConfiguration);
 
