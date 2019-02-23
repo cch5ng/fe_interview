901b47e3bee1fba7e621742cafa91ef2459a7698
Carol Chung <cch5ng@gmail.com>
2019-02-23 12:20:46 -0800

    update Questions test to use redux-mock-store and thunk

--- client/src/containers/questions/Questions.test.js
+++ client/src/containers/questions/Questions.test.js
@@ -3,23 +3,47 @@ import {createStore} from 'redux';
 import {Provider, connect} from 'react-redux';
 import {render, fireEvent, cleanup, waitForElement} from 'react-testing-library';
 import 'jest-dom/extend-expect';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk'
 import Questions from './Questions';
-//import configureMockStore from 'redux-mock-store';
-//import thunk from 'redux-thunk';
 
 jest.mock('../../utils/http_requests');
 
-function reducer(state = {tests: {}, questions: {}, auth: {}}, action) {
-  //dont need any actions at the moment
-  switch (action.type) {
-    default:
-      return state
+const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares);
+const initialState = {
+  questions: {
+    questions: {
+      "1": {
+        "id": 1,
+        "content": "How do you organize your code? (module pattern, classical inheritance?)",
+        "child_content": null,
+        "category": "JavaScript Questions",
+        "sort_order": 9
+      },
+      "2": {
+        "id": 2,
+        "content": "Explain why the following doesn't work as an IIFE: ^function foo(){ }();^.\n",
+        "child_content": "What needs to be changed to properly make it an IIFE?\n",
+        "category": "JavaScript Questions",
+        "sort_order": 4
+      },
+      "3": {
+        "id": 3,
+        "content": "Explain event delegation",
+        "child_content": null,
+        "category": "JavaScript Questions",
+        "sort_order": 0
+      }
+    }
   }
 }
 
+const store = mockStore(initialState)
+
 function renderWithRedux(
   ui,
-  {initialState, store = createStore(reducer, initialState)} = {},
+  store
 ) {
   return {
     ...render(<Provider store={store}>{ui}</Provider>),
@@ -34,38 +58,8 @@ describe('Questions', () => {
 
 	afterEach(cleanup);
 
-	const initQuestions = {
-	  questions: {
-	    questions: {
-	      "1": {
-	        "id": 1,
-	        "content": "How do you organize your code? (module pattern, classical inheritance?)",
-	        "child_content": null,
-	        "category": "JavaScript Questions",
-	        "sort_order": 9
-	      },
-	      "2": {
-	        "id": 2,
-	        "content": "Explain why the following doesn't work as an IIFE: ^function foo(){ }();^.\n",
-	        "child_content": "What needs to be changed to properly make it an IIFE?\n",
-	        "category": "JavaScript Questions",
-	        "sort_order": 4
-	      },
-	      "3": {
-	        "id": 3,
-	        "content": "Explain event delegation",
-	        "child_content": null,
-	        "category": "JavaScript Questions",
-	        "sort_order": 0
-	      }
-	    }
-	  }
-	}
-
-	it.skip('should render', () => {
-		const {getByTestId, getByText} = renderWithRedux(<Questions />, {
-			initialState: initQuestions
-		})
+	it('should render', () => {
+		const {getByTestId, getByText} = renderWithRedux(<Questions />, store)
 		let questionHeadings = document.querySelectorAll('h3');
 		expect(questionHeadings.length).toBeGreaterThan(0);
 
