cdef4fc431a3d37caacdfb3fc160bd0e85103f24
Carol Chung <cch5ng@gmail.com>
2019-02-23 20:23:50 -0800

    revert to prepopulated init state for questions list; try different forms of questions render test but the one with async action does not return view with rendered questions; mock is working

--- client/src/containers/questions/Questions.test.js
+++ client/src/containers/questions/Questions.test.js
@@ -1,52 +1,55 @@
 import React from 'react';
 import {createStore} from 'redux';
 import {Provider, connect} from 'react-redux';
-import {render, fireEvent, cleanup, waitForElement} from 'react-testing-library';
+import {render, fireEvent, cleanup, waitForElement,
+	waitForDomChange, getByTestId} from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
 import Questions from './Questions';
+import 'react-testing-library/cleanup-after-each';
 
 jest.mock('../../utils/http_requests');
 
-const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const middlewares = [thunk]; // add your middlewares like `redux-thunk`
 const mockStore = configureStore(middlewares);
-const initialState = {
-  questions: {
-    questions: {
-      "1": {
-        "id": 1,
-        "content": "How do you organize your code? (module pattern, classical inheritance?)",
-        "child_content": null,
-        "category": "JavaScript Questions",
-        "sort_order": 9
-      },
-      "2": {
-        "id": 2,
-        "content": "Explain why the following doesn't work as an IIFE: ^function foo(){ }();^.\n",
-        "child_content": "What needs to be changed to properly make it an IIFE?\n",
-        "category": "JavaScript Questions",
-        "sort_order": 4
-      },
-      "3": {
-        "id": 3,
-        "content": "Explain event delegation",
-        "child_content": null,
-        "category": "JavaScript Questions",
-        "sort_order": 0
-      }
-    }
-  }
+const initialState = //{questions: {}, tests: {}, auth: {}
+  {
+	  questions: {
+	    questions: {
+	      "1": {
+	        "id": 1,
+	        "content": "How do you organize your code? (module pattern, classical inheritance?)",
+	        "child_content": null,
+	        "category": "JavaScript Questions",
+	        "sort_order": 9
+	      },
+	      "2": {
+	        "id": 2,
+	        "content": "Explain why the following doesn't work as an IIFE: ^function foo(){ }();^.\n",
+	        "child_content": "What needs to be changed to properly make it an IIFE?\n",
+	        "category": "JavaScript Questions",
+	        "sort_order": 4
+	      },
+	      "3": {
+	        "id": 3,
+	        "content": "Explain event delegation",
+	        "child_content": null,
+	        "category": "JavaScript Questions",
+	        "sort_order": 0
+	      }
+	    }
+	  }
 }
 
-const store = mockStore(initialState)
+const store = mockStore(initialState);
 
 function renderWithRedux(
   ui,
   store
 ) {
   return {
-    ...render(<Provider store={store}>{ui}</Provider>),
+    ...render(<Provider data-testid="provier" id="provider" store={store}>{ui}</Provider>),
     // adding `store` to the returned utilities to allow us
     // to reference it in our tests (just try to avoid using
     // this to test implementation details).
@@ -54,14 +57,39 @@ function renderWithRedux(
   }
 }
 
+async function waitForElementWrap(identifier) {
+	let question = await waitForElement(
+		  () => getByTestId(container, identifier),
+		  { container }
+		)
+
+	return question
+}
+
 describe('Questions', () => {
 
 	afterEach(cleanup);
 
+	// believe this is timing out
 	it('should render', () => {
 		const {getByTestId, getByText} = renderWithRedux(<Questions />, store)
-		let questionHeadings = document.querySelectorAll('h3');
-		expect(questionHeadings.length).toBeGreaterThan(0);
+
+		const container = getByTestId('questions');
+
+		// gets stuck here, promise does not resolve
+		// let questionReturned = waitForElement(
+		//   () => getByTestId(container, 'question-item'),
+		//   { container }
+		// )
+
+		// console.log('questionReturned', questionReturned)
+		// expect(questionReturned).not.toBeNull();
+
+		// if prepopulate the store with questions (array), then Questions renders
+		// but something wrong when testing the flow with the async axn (although mock gets called as expected)
+		// maybe need to mock the action itself??
+		let question = getByTestId('question-item');
+		expect(question).not.toBeNull();
 
 	})
 
