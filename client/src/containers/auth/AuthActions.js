3b3aa47574b2e8af2eaf28e3dee686ec606404bb
Carol Chung <cch5ng@gmail.com>
2019-09-12 16:53:41 -0700

    reformat prettier

--- client/src/containers/auth/AuthActions.js
+++ client/src/containers/auth/AuthActions.js
@@ -9,106 +9,104 @@ export const REQUEST_REGISTRATION = 'REQUEST_REGISTRATION';
 export const RECEIVE_REGISTRATION = 'RECEIVE_REGISTRATION';
 
 export function requestRegistration() {
-	return {
-		type: REQUEST_REGISTRATION,
-		retrieving: true,
-		registrationError: null,
-	}
+  return {
+    type: REQUEST_REGISTRATION,
+    retrieving: true,
+    registrationError: null,
+  };
 }
 
 export function receiveRegistration(result) {
-	let userRegistered = false;
-	let registrationError = '';
-	if (result.userId) {
-		userRegistered = true;
-	}
+  let userRegistered = false;
+  let registrationError = '';
+  if (result.userId) {
+    userRegistered = true;
+  }
 
-	if (result.error) {
-		registrationError = result.error;
-	}
+  if (result.error) {
+    registrationError = result.error;
+  }
 
-	return {
-		type: RECEIVE_REGISTRATION,
-		userRegistered,
-		registrationError,
-		retrieving: false
-	}
+  return {
+    type: RECEIVE_REGISTRATION,
+    userRegistered,
+    registrationError,
+    retrieving: false,
+  };
 }
 
-export const fetchRegister = (login) => dispatch => {
-	dispatch(requestRegistration());
-	return http_requests.Auth.register(login)
-		.then(json => dispatch(receiveRegistration(json)))
-		.catch(err => console.error('fetch error', err));
-}
+export const fetchRegister = login => dispatch => {
+  dispatch(requestRegistration());
+  return http_requests.Auth.register(login)
+    .then(json => dispatch(receiveRegistration(json)))
+    .catch(err => console.error('fetch error', err));
+};
 
 // login
 export const REQUEST_LOGIN = 'REQUEST_LOGIN';
 export const RECEIVE_LOGIN = 'RECEIVE_LOGIN';
 
 export function requestLogin() {
-	return {
-		type: REQUEST_LOGIN,
-		retrieving: true,
-		loginError: null
-	}
+  return {
+    type: REQUEST_LOGIN,
+    retrieving: true,
+    loginError: null,
+  };
 }
 
 export function receiveLogin(result) {
-	let jwt = '';
-	let loginError = '';
-	let email = null;
+  let jwt = '';
+  let loginError = '';
+  let email = null;
 
-	if (result.jwt) {
-		jwt = result.jwt;
+  if (result.jwt) {
+    jwt = result.jwt;
 
+    // save jwt to localstorage
+    localStorage.setItem('fe_interview_session', jwt);
+  }
 
-		// save jwt to localstorage
-		localStorage.setItem('fe_interview_session', jwt);
-	}
+  if (result.email) {
+    email = result.email;
+  }
 
-	if (result.email) {
-		email = result.email;
-	}
+  if (result.error) {
+    loginError = result.error;
+  }
 
-	if (result.error) {
-		loginError = result.error;
-	}
-
-	return {
-		type: RECEIVE_LOGIN,
-		jwt,
-		email,
-		loginError,
-		retrieving: false
-	}
+  return {
+    type: RECEIVE_LOGIN,
+    jwt,
+    email,
+    loginError,
+    retrieving: false,
+  };
 }
 
-export const fetchLogin = (login) => dispatch => {
-	dispatch(requestLogin());
+export const fetchLogin = login => dispatch => {
+  dispatch(requestLogin());
 
-	return http_requests.Auth.login(login)
-		.then(json => dispatch(receiveLogin(json)))
-		.catch(err => console.error('fetch error', err));
-}
+  return http_requests.Auth.login(login)
+    .then(json => dispatch(receiveLogin(json)))
+    .catch(err => console.error('fetch error', err));
+};
 
 // logout
 export const LOGOUT = 'LOGOUT';
 
 export function logout() {
-	return {
-		type: LOGOUT,
-		email: ''
-	}
+  return {
+    type: LOGOUT,
+    email: '',
+  };
 }
 
 // clear login message
 export const CLEAR_LOGIN_ERROR = 'CLEAR_LOGIN_ERROR';
 
 export function clearLoginError() {
-	return {
-		type: CLEAR_LOGIN_ERROR,
-		loginError: null
-	}
+  return {
+    type: CLEAR_LOGIN_ERROR,
+    loginError: null,
+  };
 }
-
