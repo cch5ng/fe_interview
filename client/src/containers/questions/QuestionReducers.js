fd95f72ed54b81af0823f2fd66d4f7b3d69394a5
Carol Chung <cch5ng@gmail.com>
2019-09-12 22:44:20 -0700

    reformat prettier

--- client/src/containers/questions/QuestionReducers.js
+++ client/src/containers/questions/QuestionReducers.js
@@ -2,28 +2,30 @@
  * @prettier
  */
 
-import { REQUEST_ALL_QUESTIONS, RECEIVE_ALL_QUESTIONS } from './QuestionActions';
+import {
+  REQUEST_ALL_QUESTIONS,
+  RECEIVE_ALL_QUESTIONS,
+} from './QuestionActions';
 
 export function questions(state = {}, action) {
-	switch(action.type) {
-
-		case REQUEST_ALL_QUESTIONS:
-			return {
-				...state,
-				retrieving: action.retrieving
-			}
-		case RECEIVE_ALL_QUESTIONS:
-			let questionObj = {};
-			action.questions.forEach(question => {
-				let id = question.id;
-				questionObj[id] = question;
-			});
-			return {
-				...state,
-				questions: questionObj,
-				retrieving: false
-			}
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case REQUEST_ALL_QUESTIONS:
+      return {
+        ...state,
+        retrieving: action.retrieving,
+      };
+    case RECEIVE_ALL_QUESTIONS:
+      let questionObj = {};
+      action.questions.forEach(question => {
+        let id = question.id;
+        questionObj[id] = question;
+      });
+      return {
+        ...state,
+        questions: questionObj,
+        retrieving: false,
+      };
+    default:
+      return state;
+  }
 }
