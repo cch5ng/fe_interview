73dba139f5a09037034db05343dfe368f2ff5cdd
Carol Chung <cch5ng@gmail.com>
2019-09-12 17:16:11 -0700

    reformat prettier

--- client/src/containers/auth/AuthReducers.js
+++ client/src/containers/auth/AuthReducers.js
@@ -2,50 +2,55 @@
  * @prettier
  */
 
-import { REQUEST_REGISTRATION, RECEIVE_REGISTRATION,
-	REQUEST_LOGIN, RECEIVE_LOGIN, LOGOUT, CLEAR_LOGIN_ERROR } from './AuthActions';
+import {
+  REQUEST_REGISTRATION,
+  RECEIVE_REGISTRATION,
+  REQUEST_LOGIN,
+  RECEIVE_LOGIN,
+  LOGOUT,
+  CLEAR_LOGIN_ERROR,
+} from './AuthActions';
 
 export function auth(state = { userRegistered: false }, action) {
-	switch(action.type) {
-
-		case REQUEST_REGISTRATION:
-			return {
-				...state,
-				retrieving: action.retrieving,
-				registrationError: action.registrationError
-			}
-		case REQUEST_LOGIN:
-			return {
-				...state,
-				retrieving: action.retrieving,
-				loginError: action.loginError
-			}
-		case RECEIVE_REGISTRATION:
-			return {
-				...state,
-				retrieving: action.retrieving,
-				userRegistered: action.userRegistered,
-				registrationError: action.registrationError
-			}
-		case RECEIVE_LOGIN:
-			return {
-				...state,
-				retrieving: action.retrieving,
-				jwt: action.jwt,
-				email: action.email,
-				loginError: action.loginError
-			}
-		case LOGOUT:
-			return {
-				...state,
-				email: action.email
-			}
-		case CLEAR_LOGIN_ERROR:
-			return {
-				...state,
-				loginError: action.loginError
-			}
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case REQUEST_REGISTRATION:
+      return {
+        ...state,
+        retrieving: action.retrieving,
+        registrationError: action.registrationError,
+      };
+    case REQUEST_LOGIN:
+      return {
+        ...state,
+        retrieving: action.retrieving,
+        loginError: action.loginError,
+      };
+    case RECEIVE_REGISTRATION:
+      return {
+        ...state,
+        retrieving: action.retrieving,
+        userRegistered: action.userRegistered,
+        registrationError: action.registrationError,
+      };
+    case RECEIVE_LOGIN:
+      return {
+        ...state,
+        retrieving: action.retrieving,
+        jwt: action.jwt,
+        email: action.email,
+        loginError: action.loginError,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        email: action.email,
+      };
+    case CLEAR_LOGIN_ERROR:
+      return {
+        ...state,
+        loginError: action.loginError,
+      };
+    default:
+      return state;
+  }
 }
