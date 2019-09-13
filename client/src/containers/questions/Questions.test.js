b6bd5951b9747d61413ef16868928372520fb1a7
Carol Chung <cch5ng@gmail.com>
2019-09-12 22:47:39 -0700

    reformat prettier

--- client/src/containers/questions/Questions.test.js
+++ client/src/containers/questions/Questions.test.js
@@ -4,9 +4,17 @@
 
 import React from 'react';
 import { createStore, applyMiddleware, compose } from 'redux';
-import {Provider, connect} from 'react-redux';
-import {render, fireEvent, cleanup, waitForElement,
-	waitForDomChange, getByTestId, wait, getAllByTestId} from 'react-testing-library';
+import { Provider, connect } from 'react-redux';
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement,
+  waitForDomChange,
+  getByTestId,
+  wait,
+  getAllByTestId,
+} from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import thunk from 'redux-thunk';
 import Questions from './Questions';
@@ -14,31 +22,28 @@ import 'react-testing-library/cleanup-after-each';
 
 jest.mock('../../utils/http_requests');
 
-const middlewares = [thunk]; 
+const middlewares = [thunk];
 
-function reducer(state = { }, action) {
+function reducer(state = {}, action) {
   switch (action.type) {
     case 'RECEIVE_ALL_QUESTIONS':
       return {
         ...state,
         questions: {
-        	questions: action.questions
-        }
-      }
+          questions: action.questions,
+        },
+      };
     default:
-      return state
+      return state;
   }
 }
 
 function renderWithRedux(
   ui,
-  { initialState, store = createStore(reducer,
-  													initialState,
-  													compose(
-   													  applyMiddleware(thunk)
-  													)
-  												)
-	} = {}
+  {
+    initialState,
+    store = createStore(reducer, initialState, compose(applyMiddleware(thunk))),
+  } = {}
 ) {
   return {
     ...render(<Provider store={store}>{ui}</Provider>),
@@ -46,24 +51,24 @@ function renderWithRedux(
     // to reference it in our tests (just try to avoid using
     // this to test implementation details).
     store,
-  }
+  };
 }
 
 describe('Questions', () => {
+  afterEach(cleanup);
 
-	afterEach(cleanup);
+  it('should render', async () => {
+    const { getByTestId, getAllByTestId } = renderWithRedux(<Questions />, {
+      initialState: { questions: {} },
+    });
 
-	it('should render', async () => {
-		const {getByTestId, getAllByTestId} = renderWithRedux(<Questions />, {
-			initialState: {questions: {}}
-		})
+    let questionReturned = await waitForElement(() =>
+      getByTestId('question-item')
+    );
 
-		let questionReturned = await waitForElement(() => getByTestId('question-item'));
-
-		let question = getByTestId('question-item');
-		expect(question).not.toBeNull();
-		let questions = getAllByTestId('question-item');
-		expect(questions.length).toBe(2);
-	})
-
-})
+    let question = getByTestId('question-item');
+    expect(question).not.toBeNull();
+    let questions = getAllByTestId('question-item');
+    expect(questions.length).toBe(2);
+  });
+});
