c4bbf9fc1c43fc3472eb6ea8892699d80f7b26b3
Carol Chung <cch5ng@gmail.com>
2019-09-12 17:18:09 -0700

    reformat prettier

--- client/src/containers/auth/AuthActions.test.js
+++ client/src/containers/auth/AuthActions.test.js
@@ -4,8 +4,14 @@
 
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { REQUEST_REGISTRATION, RECEIVE_REGISTRATION, fetchRegister,
-	REQUEST_LOGIN, RECEIVE_LOGIN, fetchLogin } from './AuthActions';
+import {
+  REQUEST_REGISTRATION,
+  RECEIVE_REGISTRATION,
+  fetchRegister,
+  REQUEST_LOGIN,
+  RECEIVE_LOGIN,
+  fetchLogin,
+} from './AuthActions';
 
 jest.mock('../../utils/http_requests');
 
@@ -13,86 +19,82 @@ const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe('Async Auth Actions', () => {
-
   const registration = {
-  	email: 'dummy@d.com',
-  	password: 'password'
-  }
+    email: 'dummy@d.com',
+    password: 'password',
+  };
 
-	describe('fetch register ', () => {
-		const expectedActions = [
-	    { type: REQUEST_REGISTRATION, retrieving: true,
-	    	registrationError: null
-	    },
-	    { type: RECEIVE_REGISTRATION, retrieving: false,
-				userRegistered: true,
-				registrationError: '',
-			}
-	  ];
+  describe('fetch register ', () => {
+    const expectedActions = [
+      { type: REQUEST_REGISTRATION, retrieving: true, registrationError: null },
+      {
+        type: RECEIVE_REGISTRATION,
+        retrieving: false,
+        userRegistered: true,
+        registrationError: '',
+      },
+    ];
 
-		const expectedActionsFail = [
-	    { type: REQUEST_REGISTRATION, retrieving: true,
-	    	registrationError: null
-	    },
-	    { type: RECEIVE_REGISTRATION, retrieving: false,
-				userRegistered: false,
-				registrationError: 'registration could not be completed',
-			}
-	  ];
+    const expectedActionsFail = [
+      { type: REQUEST_REGISTRATION, retrieving: true, registrationError: null },
+      {
+        type: RECEIVE_REGISTRATION,
+        retrieving: false,
+        userRegistered: false,
+        registrationError: 'registration could not be completed',
+      },
+    ];
 
-		it('fetch register should return', () => {
-			const store = mockStore({ auth: {} })
-	    return store.dispatch(fetchRegister(registration)).then(() => {
-	      expect(store.getActions()).toEqual(expectedActions)
-	    })
-		})
+    it('fetch register should return', () => {
+      const store = mockStore({ auth: {} });
+      return store.dispatch(fetchRegister(registration)).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+    });
 
-		it('fetch register should handle error', () => {
-			const store = mockStore({ auth: {} })
-	    return store.dispatch(fetchRegister()).then(() => {
-	      expect(store.getActions()).toEqual(expectedActionsFail)
-	    })
-		})
-	})
+    it('fetch register should handle error', () => {
+      const store = mockStore({ auth: {} });
+      return store.dispatch(fetchRegister()).then(() => {
+        expect(store.getActions()).toEqual(expectedActionsFail);
+      });
+    });
+  });
 
-	describe('fetch login ', () => {
-		const expectedLoginActions = [
-	    { type: REQUEST_LOGIN, retrieving: true,
-	    	loginError: null
-	    },
-	    { type: RECEIVE_LOGIN, retrieving: false,
-				jwt: 'dummy token',
-				email: 'zzz@z.com',
-				loginError: ''
-			}
-	  ];
+  describe('fetch login ', () => {
+    const expectedLoginActions = [
+      { type: REQUEST_LOGIN, retrieving: true, loginError: null },
+      {
+        type: RECEIVE_LOGIN,
+        retrieving: false,
+        jwt: 'dummy token',
+        email: 'zzz@z.com',
+        loginError: '',
+      },
+    ];
 
-		it('fetch login should return', () => {
-			const store = mockStore({ auth: {} })
-	    return store.dispatch(fetchLogin(registration)).then(() => {
-	      expect(store.getActions()).toEqual(expectedLoginActions)
-	    })
-		})
+    it('fetch login should return', () => {
+      const store = mockStore({ auth: {} });
+      return store.dispatch(fetchLogin(registration)).then(() => {
+        expect(store.getActions()).toEqual(expectedLoginActions);
+      });
+    });
 
-		const expectedLoginActionsFail = [
-	    { type: REQUEST_LOGIN, retrieving: true,
-	    	loginError: null
-	    },
-	    { type: RECEIVE_LOGIN, retrieving: false,
-				jwt: '',
-				email: null,
-				loginError: 'login could not be completed'
-			}
-	  ];
+    const expectedLoginActionsFail = [
+      { type: REQUEST_LOGIN, retrieving: true, loginError: null },
+      {
+        type: RECEIVE_LOGIN,
+        retrieving: false,
+        jwt: '',
+        email: null,
+        loginError: 'login could not be completed',
+      },
+    ];
 
-		it('fetch login should handle error', () => {
-			const store = mockStore({ auth: {} })
-	    return store.dispatch(fetchLogin()).then(() => {
-	      expect(store.getActions()).toEqual(expectedLoginActionsFail)
-	    })
-		})
-	})
-
-
-
-})
+    it('fetch login should handle error', () => {
+      const store = mockStore({ auth: {} });
+      return store.dispatch(fetchLogin()).then(() => {
+        expect(store.getActions()).toEqual(expectedLoginActionsFail);
+      });
+    });
+  });
+});
