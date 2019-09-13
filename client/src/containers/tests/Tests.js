d3f114c78dc1078d1ff7e2d06a74ef82545e8065
Carol Chung <cch5ng@gmail.com>
2019-09-12 23:00:58 -0700

    reformat prettier

--- client/src/containers/tests/Tests.js
+++ client/src/containers/tests/Tests.js
@@ -15,108 +15,123 @@ let styles = {};
 Object.assign(styles, globalStyles, testStyles);
 
 class Tests extends Component {
+  constructor(props) {
+    super(props);
 
-	constructor(props) {
-		super(props);
+    this.getQuestionsMissedCount = this.getQuestionsMissedCount.bind(this);
+    this.getAvgTimePerQuestion = this.getAvgTimePerQuestion.bind(this);
+    this.prettyMissedQuestionsStr = this.prettyMissedQuestionsStr.bind(this);
+  }
 
-		this.getQuestionsMissedCount = this.getQuestionsMissedCount.bind(this);
-		this.getAvgTimePerQuestion = this.getAvgTimePerQuestion.bind(this);
-		this.prettyMissedQuestionsStr = this.prettyMissedQuestionsStr.bind(this);
-	}
+  componentDidMount() {
+    let email =
+      this.props.auth && this.props.auth.email ? this.props.auth.email : null;
+    if (email) {
+      this.props.dispatch(fetchTests({ email }));
+    }
+  }
 
-	componentDidMount() {
-		let email = this.props.auth && this.props.auth.email ? this.props.auth.email : null;
-		if (email) {
-			this.props.dispatch(fetchTests({ email }));
-		}
-	}
+  getQuestionsMissedCount(questionsAr) {
+    let missedCount = 0;
 
-	getQuestionsMissedCount(questionsAr) {
-		let missedCount = 0;
-		
-		questionsAr.forEach(question => {
-			if (question.status !== 'completed') {
-				missedCount += 1;
-			}
-		});
+    questionsAr.forEach(question => {
+      if (question.status !== 'completed') {
+        missedCount += 1;
+      }
+    });
 
-		return missedCount;
-	}
+    return missedCount;
+  }
 
-	getAvgTimePerQuestion(totalTime, completedQuestCount) {
-		if (completedQuestCount === 0) {
-			return 0;
-		}
+  getAvgTimePerQuestion(totalTime, completedQuestCount) {
+    if (completedQuestCount === 0) {
+      return 0;
+    }
 
-		let avgTime = totalTime / completedQuestCount;
+    let avgTime = totalTime / completedQuestCount;
 
-		return getPrettyTime(avgTime);
-	}
+    return getPrettyTime(avgTime);
+  }
 
-	prettyMissedQuestionsStr(missedQuestionsCnt) {
-		if (missedQuestionsCnt === 0) {
-			return 'all questions completed 😆🍕';
-		}
-		if (missedQuestionsCnt === 1) {
-			return '1 question not completed';
-		}
-		return `${missedQuestionsCnt} questions not completed 😬`;
-	}
+  prettyMissedQuestionsStr(missedQuestionsCnt) {
+    if (missedQuestionsCnt === 0) {
+      return 'all questions completed 😆🍕';
+    }
+    if (missedQuestionsCnt === 1) {
+      return '1 question not completed';
+    }
+    return `${missedQuestionsCnt} questions not completed 😬`;
+  }
 
-	render() {
-		let { tests } = this.props;
-		let testsAr = [];
-		let testError = tests && tests.testError ? tests.testError : null;
+  render() {
+    let { tests } = this.props;
+    let testsAr = [];
+    let testError = tests && tests.testError ? tests.testError : null;
 
-		if (tests && tests.tests) {
-			testsAr = Object.keys(tests.tests).map(testKey => {
-				return this.props.tests.tests[testKey];
-			})
-		}
+    if (tests && tests.tests) {
+      testsAr = Object.keys(tests.tests).map(testKey => {
+        return this.props.tests.tests[testKey];
+      });
+    }
 
-		if (this.props.auth && !this.props.auth.email) {
-			return (<Redirect to="/login" />)
-		}
+    if (this.props.auth && !this.props.auth.email) {
+      return <Redirect to="/login" />;
+    }
 
-		return (
-			<div className={styles.testsContainer}>
-				<h1>All Tests</h1>
+    return (
+      <div className={styles.testsContainer}>
+        <h1>All Tests</h1>
 
-				{testError && (
-					<div className={styles.errorMessage}>
-						{testError}
-					</div>
-				)}
+        {testError && <div className={styles.errorMessage}>{testError}</div>}
 
-				{testsAr.map(test => {
-					const missedQuestions = test.questions ? this.getQuestionsMissedCount(test.questions) : null;
-					const completedQuestionsCount = test.questions ? test.questions.length - missedQuestions : null;
-					const testSummaryUrl = `/tests/${test.id}`;
+        {testsAr.map(test => {
+          const missedQuestions = test.questions
+            ? this.getQuestionsMissedCount(test.questions)
+            : null;
+          const completedQuestionsCount = test.questions
+            ? test.questions.length - missedQuestions
+            : null;
+          const testSummaryUrl = `/tests/${test.id}`;
 
-					return (
-						<div className={styles.test} key={test.id} data-testid="test-item">
-							<Link to={testSummaryUrl}>
-								<ul>
-									<li className={[styles.testsListDetail, styles.testName].join(' ')}>{test.name}</li>
-									<li className={styles.testsListDetail}>Created on {getPrettyDate(test.date_taken)}</li>
-									<li className={styles.testsListDetail}>{test.questions ? test.questions.length : null} questions</li>
-									<li className={styles.testsListDetail}>{this.prettyMissedQuestionsStr(missedQuestions)}</li>
-									<li className={styles.testsListDetail}>Completed in {getPrettyTime(test.time_total - test.time_remaining)} / {getPrettyTime(test.time_total)}</li>
-								</ul>
-							</Link>
-						</div>
-					)	
-				})}
-			</div>
-		)
-	}
+          return (
+            <div className={styles.test} key={test.id} data-testid="test-item">
+              <Link to={testSummaryUrl}>
+                <ul>
+                  <li
+                    className={[styles.testsListDetail, styles.testName].join(
+                      ' '
+                    )}>
+                    {test.name}
+                  </li>
+                  <li className={styles.testsListDetail}>
+                    Created on {getPrettyDate(test.date_taken)}
+                  </li>
+                  <li className={styles.testsListDetail}>
+                    {test.questions ? test.questions.length : null} questions
+                  </li>
+                  <li className={styles.testsListDetail}>
+                    {this.prettyMissedQuestionsStr(missedQuestions)}
+                  </li>
+                  <li className={styles.testsListDetail}>
+                    Completed in{' '}
+                    {getPrettyTime(test.time_total - test.time_remaining)} /{' '}
+                    {getPrettyTime(test.time_total)}
+                  </li>
+                </ul>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		tests: state.tests,
-		auth: state.auth,
-	}
+  return {
+    tests: state.tests,
+    auth: state.auth,
+  };
 }
 
 export default connect(mapStateToProps)(Tests);
