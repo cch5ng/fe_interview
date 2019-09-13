a72d94d0ad382b176b8b9a84fd6ce307eb7dcbc3
Carol Chung <cch5ng@gmail.com>
2019-09-13 11:58:31 -0700

    reformat prettier

--- client/src/utils/http_requests.js
+++ client/src/utils/http_requests.js
@@ -14,64 +14,63 @@ const TEST_QUESTION_UPDATE = 'test/updateQuestion';
 
 const requests = {
   get: url => {
-  	return new Promise((resolve, reject) => {
-			fetch(url)
-				.then(resp => resolve(resp.json()))
-				.catch(err => reject(err))
-					//console.error('fetch error', err));
-  	})
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(resp => resolve(resp.json()))
+        .catch(err => reject(err));
+      //console.error('fetch error', err));
+    });
   },
   post: (url, body) => {
-  	return new Promise((resolve, reject) => {
-			fetch(url,
-				{	method: 'POST',
-					headers: {
-	            "Content-Type": "application/json",
-	        },
-	        body: JSON.stringify(body),
-				}
-			)
-				.then(resp => resolve(resp.json()))
-				.catch(err => reject(err))
-  	})
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      })
+        .then(resp => resolve(resp.json()))
+        .catch(err => reject(err));
+    });
   },
 };
 
 const Questions = {
-	getAll: () => {
-		return requests.get(`${API_ROOT}${QUESTIONS_ALL}`);
-	},
-	getRandomQuestions: (body) => {
-		return requests.post(`${API_ROOT}${QUESTIONS_RANDOM}`, body);
-	}
-}
+  getAll: () => {
+    return requests.get(`${API_ROOT}${QUESTIONS_ALL}`);
+  },
+  getRandomQuestions: body => {
+    return requests.post(`${API_ROOT}${QUESTIONS_RANDOM}`, body);
+  },
+};
 
 const Auth = {
-	register: (body) => {
-		return requests.post(`${API_ROOT}${AUTH_REGISTER}`, body);
-	},
-	login: (body) => {
-		return requests.post(`${API_ROOT}${AUTH_LOGIN}`, body);
-	}
-}
+  register: body => {
+    return requests.post(`${API_ROOT}${AUTH_REGISTER}`, body);
+  },
+  login: body => {
+    return requests.post(`${API_ROOT}${AUTH_LOGIN}`, body);
+  },
+};
 
 const Tests = {
-	getAll: (body) => {
-		return requests.post(`${API_ROOT}${TESTS_ALL}`, body);
-	},
-	getTestById: (body) => {
-		return requests.post(`${API_ROOT}${TEST_BY_ID}`, body);
-	},
-	initializeTest: (body) => {
-		return requests.post(`${API_ROOT}${TEST_INIT}`, body);
-	},
-	updateTestQuestion: (body) => {
-		return requests.post(`${API_ROOT}${TEST_QUESTION_UPDATE}`, body);		
-	}
-}
+  getAll: body => {
+    return requests.post(`${API_ROOT}${TESTS_ALL}`, body);
+  },
+  getTestById: body => {
+    return requests.post(`${API_ROOT}${TEST_BY_ID}`, body);
+  },
+  initializeTest: body => {
+    return requests.post(`${API_ROOT}${TEST_INIT}`, body);
+  },
+  updateTestQuestion: body => {
+    return requests.post(`${API_ROOT}${TEST_QUESTION_UPDATE}`, body);
+  },
+};
 
 export default {
-	Questions,
-	Auth,
-	Tests,
-}
+  Questions,
+  Auth,
+  Tests,
+};
